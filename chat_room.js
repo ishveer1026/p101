34f2476193d19bf290da020b24347a6ae85b545c
ishveer1026 <107894915+ishveer1026@users.noreply.github.com>
2022-11-07 16:32:50 -0800

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDoUKfT2Yirgsx6pOXybGQanfw1ukX5dTc",
+    authDomain: "chat-app-bb1f2.firebaseapp.com",
+    databaseURL: "https://chat-app-bb1f2-default-rtdb.firebaseio.com",
+    projectId: "chat-app-bb1f2",
+    storageBucket: "chat-app-bb1f2.appspot.com",
+    messagingSenderId: "433599892860",
+    appId: "1:433599892860:web:e3a42fe5c577e14e9268f9"
+  };
+  
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
